28be8146b9cc697c13c9b5786217aaa8f2ae5152
DINESH N <78141949+DINESHN27@users.noreply.github.com>
2022-02-26 08:47:28 +0530

    Add files via upload

---
+++ Assignment(1.1).py
@@ -0,0 +1,40 @@
+#USAGE OF ALL OPERATORS
+x=int(input("Enter the value for x: "))
+y=int(input("Enter the value for y: "))
+print("{} + {} = {}".format(x,y,x+y))
+print("{} - {} = {}".format(x,y,x-y))
+print("{} * {} = {}".format(x,y,x*y))
+print("{} / {} = {}".format(x,y,x/y))
+print("{} % {} = {}".format(x,y,x%y))
+print("{} ** {} = {}".format(x,y,x**y))
+print("{} // {} = {}".format(x,y,x//y))
+
+
+#ALGORITHM:
+
+#step1: Start
+#step2: n = Previous number of x
+#step3: y = n-2
+#step4: m = previous number of n
+#step5: x = m+2
+#step6: print X and Y
+#step7: Stop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
